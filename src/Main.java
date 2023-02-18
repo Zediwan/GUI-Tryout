a5934c12dbdad6f11e6a1f22feac0c164e9dc135
Jeremy Moser <Jeremy@qualit.ch>
2023-02-18 14:39:31 +0100

    Chapter 7 Layered Pane

--- src/Main.java
+++ src/Main.java
@@ -161,6 +161,40 @@ public class Main {
         frame.add(panel);
         frame.setVisible(true);
          */
+        /*
+        Chapter 7: Grid Layout Manager
 
+        JLabel label1 = new JLabel();
+        JLabel label2 = new JLabel();
+        JLabel label3 = new JLabel();
+
+        label1.setOpaque(true);
+        label1.setBackground(Color.red);
+        label1.setBounds(50,50,200,200);
+
+        label2.setOpaque(true);
+        label2.setBackground(Color.green);
+        label2.setBounds(100,100,200,200);
+
+        label3.setOpaque(true);
+        label3.setBackground(Color.blue);
+        label3.setBounds(150,150,200,200);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,500,500);
+        layeredPane.add(label1, Integer.valueOf(0));
+        layeredPane.add(label2, Integer.valueOf(4));
+        layeredPane.add(label3, Integer.valueOf(3));
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500,500));
+        frame.setLayout(null);
+        frame.setVisible(true);
+         */
+        /*
+        Chapter 8: new Windows
+         */
     }
 }
